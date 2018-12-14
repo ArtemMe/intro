e8d6fb46bdaa80d66d007e120836dadf11bf9bcb
Mezhelovsky Artem <lbattersu@gmail.com>
2018-12-14 23:01:16 +0300

    Updates

---
+++ precache-manifest.00fa6da476577e2010c0696f3fe8d6f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/intro/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7b2d56c37534c56a3b31",
+    "url": "/intro/static/js/runtime~main.7b2d56c3.js"
+  },
+  {
+    "revision": "d04baacdd8a883e7f7d2",
+    "url": "/intro/static/js/main.d04baacd.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/intro/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "d04baacdd8a883e7f7d2",
+    "url": "/intro/static/css/main.00d35551.chunk.css"
+  },
+  {
+    "revision": "331185fe78d17f461ea9cc2c9d99f383",
+    "url": "/intro/index.html"
+  }
+];
